39d8cec13a94c9c03c658a256988f6033ba49185
Nayaka Yoga Pradipta <nayakayp@gmail.com>
2024-12-17 10:45:39 +0700

    Update index.js
    test

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
+import { useEffect } from 'react';
 
 import Property from "../components/Property";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
@@ -36,6 +37,20 @@ const Banner = ({
 );
 
 export default function Home({ propertiesForSale, propertiesForRent }) {
+    const fetchAvailableDate = async () => {
+    await fetch('https://portal.link2feed.ca/org/650/appointment/3827792/program/4277/availability?start_date=2024-12-17&end_date=2024-12-31&location=Any&see_more=true', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cookie': 'L2FSessionID=05f1a99f42bd4e79255d57e41dbff6a1; sdc_production_can=RzhyM3E2UTBvSTN3WUVPWlFHZlFkVERuYWk5d2gra3JPTXQwc3F1VFRrZCtwRkZ0K0hlbi80endWWG9sWHFIUUNZS0todlZJYkFmMHlveHROME5PVXQrZm03dWlxM2ZUV1FQUk9ZZzB6VWNIUWFhTDVReG9TZUFjc1l3NWdqUzQ5ZXFvWGg1d200MGVhckNPbTAwQVpzbFJLckU5VmZaM1pVNzVqNUtRRkUwZGoxZHIwaUdCSzJXeHkzczNheXZaMW5GZzdXaVBuT2VIcjNoc2FNdGdmOWpJTzFUTEgvV0FReC93NzhORTVYWEZOZEhDeUtNSVRocmVLOU9qVmpYTSs0SVl3T1J3Qy93VVliWnNkcHpleEwrVk0rOTdxQkpHL3dScTFpZ0ZxMmVlUGxqTU90OG8zZ09sRzNDamJjaUE4b3hPSlJ4YlBRZTNNUjQvOXl2RUx3PT0tLU1xLzRYbHRURmxpVUJFanMvYW5Ob0E9PQ%3D%3D--808d3d7acaf4b8605d00dbce13db69e7da04521b'
+      },
+    })
+  }
+
+  useEffect(() => {
+    fetchAvailableDate()
+  }, [])
+
   return (
     <Box>
       <Banner
